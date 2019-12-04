3f1a964c1ff9330bb06ef093631340678194bb97
MarconeAugusto <marcone.p.louzada@gmail.com>
2019-12-04 19:59:03 -0300

    up

--- ex3.m
+++ ex3.m
@@ -1,52 +1,55 @@
-clear all;
-close all;
-clc;
+clear all
+close all
+clc
+
+% parametros do canal
+fd = 100;
+Rs = 10e3;
 
 M = 2;
-fd = 100;   %100 hertz, mobilidade elevada
-Rs = 10e3;  %taxa de simbolo
+info = randi([0 M-1], 1e6, 1);
+info_mod = pskmod(info, M);
 
-info = randi([0 M-1], 4 , 1);
-info_mod = pskmod(info,M);
+info_mod_i = info_mod(1:2:end); % impares
+info_mod_p = info_mod(2:2:end); % pares
 
-%separa vetor em pares e impares
-info_mod_i = info_mod(1:2:end); %impar
-info_mod_p = info_mod(2:2:end); %par
+info_tx_1 = zeros(1, length(info));
+info_tx_2 = zeros(1, length(info));
 
-info_tx_0 = zeros(1,length(info)); %cria vetor de zeros
-info_tx_1 = zeros(1,length(info)); %cria vetor de zeros
+info_tx_1(1:2:end) = info_mod_i;
+info_tx_1(2:2:end) = -conj(info_mod_p);
+info_tx_2(1:2:end) = info_mod_p;
+info_tx_2(2:2:end) = conj(info_mod_i);
 
-%separar os vetores
-info_tx_0(1:2:end) = info_mod_i;
-info_tx_0(2:2:end) = -conj(info_mod_p);
-info_tx_1(1:2:end) = info_mod_p;
-info_tx_1(2:2:end) = conj(info_mod_i);
-
-%criar os canais
-canal1 = rayleighchan(1/Rs, fd); %canal 1
+% canal sendo criado
+canal1 = rayleighchan(1/Rs, fd);
 canal1.StoreHistory = 1;
-canal2 = rayleighchan(1/Rs, fd); %canal 2
+canal2 = rayleighchan(1/Rs, fd);
 canal2.StoreHistory = 1;
 
-%envia a informação modulada
-h0 = filter(canal1, info_tx_0);
-ganho_canal1 = canal1.PathGains;    %ganho canal 1
-h1 = filter(canal2, info_tx_1);
-ganho_canal2 = canal2.PathGains;    %ganho canal 1
-sinal_rx0 = (h0.*info_tx_0) + (h1.*info_tx_1);
-sinal_rx0 = sinal_rx0  + awgn(sinal_rx0, 10);
-sinal_rx1 = -(h0.*conj(info_tx_1)) + (h1.*conj(info_tx_0));
-sinal_rx1 = sinal_rx1 + awgn(sinal_rx1, 20);
+% filter representa o processo de transmissao
+sinal_rx1 = filter(canal1, info_tx_1);
+ganho_canal1 = canal1.PathGains;
+sinal_rx2 = filter(canal2, info_tx_2);
+ganho_canal2 = canal2.PathGains;
+sinal_rx = sinal_rx1 + sinal_rx2;
 
-s0 = conj(h0).*sinal_rx0 + h1.*conj(sinal_rx1);
-s1 = conj(h1).*sinal_rx0 - h0.*conj(sinal_rx0);
+% soma de awgn em diversas condicoes (SNR variando)
+for SNR = 0:25
+    sinal_rx_awgn = transpose(awgn(sinal_rx, SNR));
+    
+    % equalizacao (remover giro da constelacao)
+    s1 = (conj(ganho_canal1(1:2:end)).*sinal_rx_awgn(1:2:end)) + (ganho_canal2(2:2:end).*conj(sinal_rx_awgn(2:2:end)));
+    s2 = (conj(ganho_canal2(1:2:end)).*sinal_rx_awgn(1:2:end)) - (ganho_canal1(2:2:end).*conj(sinal_rx_awgn(2:2:end)));
+    
+    sinal_demod = zeros(1, length(info));
+    sinal_demod(1:2:end) = s1;
+    sinal_demod(2:2:end) = s2;
+    sinal_demod_alam = pskdemod(sinal_demod, 2);
+    
+    [num(SNR+1), taxa(SNR+1)] = biterr(info, transpose(sinal_demod_alam));
+end
 
-s0_demod = pskdemod(s0,M);
-s1_demod = pskdemod(s1,M);
-
-figure(1)
-plot(20*log10(abs(ganho_canal1)),'b');grid minor;hold on
-plot(20*log10(abs(ganho_canal2)),'r');
-title('Sinal recebido'); ylabel('Potência (dBm)');xlabel('tempo (s)');
-legend('Ganho Rx_1', 'Ganho Rx_2')
-
+figure
+semilogy([0:25],taxa,'b');grid minor;
+title('Desempenho'); ylabel('BER');xlabel('SNR [dB]');
